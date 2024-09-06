11d7f5da951e1ff083c8511e98d6b3ac7ac8ddb1
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2024-09-06 03:28:45 +0100

    Computed Error rate

--- linear-regression/main.py
+++ linear-regression/main.py
@@ -1,5 +1,20 @@
 from numpy import *
 
+def computeErrorForLineGivenPoints(c, m, points):
+     #Initialize Error at 0
+     totalError = 0
+
+     for i in range(0, len(points)):
+          #Get 'x' value
+          x = points[i, 0]
+          #Get 'y' value
+          y = points[i, 1]
+          #Get differemce , square it, then add it to the total
+          totalError += (y -(m * x + c)) ** 2
+
+     #Return the Average
+     return totalError / float(len(points))
+
 def run():
      
      #Step 1 - Collect data
@@ -14,6 +29,8 @@ def run():
 
      #Step 3 - Train our model
      print(f"Starting Gradient Descent at [ c ] = {initialC}, [ m ] = {initialM}, [ error ] = {computeErrorForLineGivenPoints(initialC, initialM, points)}")
+     [c, m] = gradientDescentRunner(points, initialC, initialM, learningRate, numOfIterations)
+     print(f"Ending Point at [ c ] = {c}, [ m ] = {m}, [ error ] = {computeErrorForLineGivenPoints(c, m, points)}")
 
 
 
