25d0fc7d53594f9fe79427160458a901f1d3b125
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2024-10-30 01:06:11 +0000

    First local project

---
+++ pyForML/textAnalyser.py
@@ -0,0 +1,35 @@
+#
+#     TEXT ANLYSER
+#
+
+from collections import Counter
+import matplotlib.pyplot as plt
+
+
+def anaylseText(text):
+     words = text.split()
+     wordFrequency = Counter(words)
+     uniqueWords = set(words)
+
+     return [wordFrequency, uniqueWords]
+
+def outputAnalysis(wordFrequency, uniqueWords):
+     return f""" 
+     Word Frequency: {wordFrequency}
+     Unique Words: {uniqueWords}
+     """
+
+def plotAnalysis(wordFrequency, uniqueWords):
+     freq = wordFrequency.values()
+
+     plt.bar(uniqueWords, freq)
+     plt.xlabel('Words')
+     plt.ylabel('Frequency')
+     plt.title('Frequency of Words')
+     plt.show()
+
+userInput = input("Input text to be analysed: ")
+output = anaylseText(userInput)
+
+print(outputAnalysis(output[0], output[1]))
+plotAnalysis(output[0], list(output[1]))
